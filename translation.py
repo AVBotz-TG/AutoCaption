920298d51a96c3fe3233b422686f268532cfb2ae
Animesh Verma <85546858+AVBotz-TG@users.noreply.github.com>
2021-07-10 00:14:54 +0530

    Create translation.py

---
+++ translation.py
@@ -0,0 +1,47 @@
+class Translation(object):
+
+START_TEXT = """
+😃 Hai {},
+I am a simple auto caption bot
+I can automatically add pre-setted caption to the files in channels
+I am currently working for a channel if you want to use me, Use my source code and Deploy me
+⚜️ Maintained By 👉 @{}
+"""    
+
+HELP_TEXT = """
+<u>💡 𝐇𝐞𝐥𝐩</u> 
+👉 Add me as a Admin in your channel with edit permission
+👉 Add your caption using <code>/set_caption</code> command
+<i>( Only Admin )</i>
+   
+<i>(Support Markdown)</i>
+👉 Forward your files in your channel and I will edit it
+<b><a href="https://t.me/Ts_bots">©️ ᴛʀᴀᴄᴋsᴛᴜᴅɪᴏ's ʙᴏᴛs</a></b>
+"""    
+
+ABOUT_TEXT = """
+📕 --𝐀𝐛𝐨𝐮𝐭-- --𝐌𝐞--
+○ ᴍʏ ɴᴀᴍᴇ : [Auto Caption | Ts Bots](https://t.me/Anylink_Movies)
+○ ʟᴀɴɢᴜᴀɢᴇ : ᴘʏᴛʜᴏɴ 
+○ ғʀᴀᴍᴇᴡᴏʀᴋ : ᴘʏʀᴏɢʀᴀᴍ 
+○ sᴇʀᴠᴇʀ : ʜᴇʀᴏᴋᴜ 
+○ ᴠᴇʀsɪᴏɴ : 1.1.0
+○ sᴏᴜʀᴄᴇ ᴄᴏᴅᴇ  : [ᴛᴏᴜᴄʜ ᴍᴇ 🤗](https://github.com/Ts-bots/AutoCaption-Ts_Bot-Clone)
+○ ᴄʀᴇᴀᴛᴏʀ :  [ᴛʀᴀᴄᴋsᴛᴜᴅɪᴏ](https://t.me/trackstudio)
+ 
+__ᴘʟᴇᴀsᴇ ɴᴏᴛᴇ ɪ ᴀᴍ ɴᴏᴛ ᴀ ᴘʀᴏ ᴄᴏᴅᴇʀ ᴀɴᴅ ᴛʜɪs ɪs ᴍʏ ғɪʀsᴛ ᴛɪᴍᴇ ɪ ᴀᴍ ɢɪᴠɪɴɢ ᴀɴ ᴏᴘᴇɴ sᴏᴜʀᴄᴇ ᴄᴏᴅᴇ ᴏғ ʙᴏᴛ ғᴏʀ ᴇᴠᴇʀʏᴏɴᴇ ᴡʜɪᴄʜ ᴡᴀs ᴡʀɪᴛᴇʀ ɪɴ ᴍʏ ᴏᴡɴ ʜᴀɴᴅs, sᴏ ɪғ ᴀɴʏ ʙᴜɢs ғᴇᴇʟ ғʀᴇᴇ ᴛᴏ sᴀʏ
+ɪᴛ ᴍᴀʏ ʙᴇ ɴᴏᴛ ᴛʜᴇ ʙᴇsᴛ ʙᴜᴛ ғᴏʀ ᴍᴇ ɪᴛ ɪs ᴛʜᴇ ʙᴇsᴛ 🥰
+ᴛʜᴀɴᴋs ᴛᴏ ᴍʏ ʙᴇʟᴏᴠᴇᴅ ғʀɪᴇɴᴅ [ᴀɴᴏɴʏᴍᴏᴜs](https://t.me/Anoymous_Ns)__
+**[©️ ᴛʀᴀᴄᴋsᴛᴜᴅɪᴏ's ʙᴏᴛs](https://t.me/ts_bots)**
+"""
+
+MARKDOWN_TEXT = """
+<b>👀 You can also use Markdown Filters and Buttons for your captions...! 
+
+📝 <code>**Bold Text**</code> 
+📝 <code>__text__</code> 
+📝 <code>`text`</code>
+📝 <code>[Hyperlink Text](https://t.me/AVBotz)</code>
+
+© Made by @AVBotz ❤️</b>
+"""
